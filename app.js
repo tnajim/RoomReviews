d4772acba833246084ea38b7de4f969bcb19507f
Taher Najim <taher.najim@yahoo.com>
2023-04-19 17:09:22 +0800

    added create and post route to add hotels

--- app.js
+++ app.js
@@ -13,19 +13,36 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+app.use(express.urlencoded({ extended: true }));
+
 app.get('/', (req, res) => {
     res.render('home')
 })
 
+// index route
 app.get('/hotels', async (req, res) => {
     const hotels = await HotelModel.find({});
     res.render('hotels/index', { hotels })
 })
 
+// create hotel route
+app.get('/hotels/new', (req, res) => {
+    res.render('hotels/new');
+})
+
+// create hotel post route
+app.post('/hotels', async (req, res) => {
+    const hotel = new HotelModel(req.body.hotel);
+    await hotel.save();
+    res.redirect(`/hotels/${hotel._id}`);
+})
+
+// show hotel route
 app.get('/hotels/:id', async (req, res) => {
     const hotel = await HotelModel.findById(req.params.id);
     res.render('hotels/show', { hotel });
 })
+
 
 
 // listen to server
