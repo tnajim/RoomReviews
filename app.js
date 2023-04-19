aea75bf19cdc82843901d209d0c046d1320db5c1
Taher Najim <taher.najim@yahoo.com>
2023-04-19 10:46:05 +0800

    created connection to mongo database

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 const express = require('express');
-const app = express();
 const path = require('path');
+const mongoose = require('mongoose');
+const HotelModel = require('./models/hotel');
+
+
+mongoose.connect('mongodb://127.0.0.1:27017/room-reviews')
+    .then(() => console.log("MongoDB Connection Established."))
+    .catch(error => console.log("MongoDB Error Occured.", error));
+
+const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
@@ -9,6 +17,14 @@ app.get('/', (req, res) => {
     res.render('home')
 })
 
+app.get('/makehotel', async (req, res) => {
+    const hotel = new HotelModel({ title: 'My Room', description: 'cheap room' });
+    await hotel.save();
+    res.send(hotel);
+})
+
+
+// listen to server
 app.listen(3000, () => {
     console.log("Listening on Port 3000");
 })
