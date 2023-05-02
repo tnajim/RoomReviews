db8b143a5408b617c191a516ed6d1b0a81e98865
Taher Najim <taher.najim@yahoo.com>
2023-05-02 16:54:17 +0800

    added the review model and updated crud accordingly

--- app.js
+++ app.js
@@ -2,11 +2,12 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
-const {hotelSchema} = require('./schemas.js');
+const { hotelSchema, reviewSchema } = require('./schemas.js');
 const catchAsync = require('./utils/catchAsync');
 const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
 const HotelModel = require('./models/hotel');
+const Review = require('./models/review');
 
 
 mongoose.connect('mongodb://127.0.0.1:27017/room-reviews')
@@ -33,21 +34,29 @@ const validateHotel = (req, res, next) => {
     }
 }
 
+const validateReview = (req, res, next) => {
+    const { error } = reviewSchema.validate(req.body);
+    if (error) {
+        console.log(error);
+        const msg = error.details.map(el => el.message).join(',');
+        throw new ExpressError(msg, 400);
+    } else {
+        next();
+    }
+}
+
 app.get('/', (req, res) => {
     res.render('home')
 })
-
 // index get route
 app.get('/hotels', catchAsync(async (req, res) => {
     const hotels = await HotelModel.find({});
     res.render('hotels/index', { hotels })
 }))
-
 // create new hotel get route
 app.get('/hotels/new', (req, res) => {
     res.render('hotels/new');
 })
-
 // create new hotel post route
 app.post('/hotels', validateHotel, catchAsync(async (req, res, next) => {
     // if (!req.body.hotel) throw new ExpressError('Invalid Hotel Data', 400);
@@ -55,30 +64,43 @@ app.post('/hotels', validateHotel, catchAsync(async (req, res, next) => {
     await hotel.save();
     res.redirect(`/hotels/${hotel._id}`);
 }))
-
 // show one hotel get route
 app.get('/hotels/:id', catchAsync(async (req, res) => {
-    const hotel = await HotelModel.findById(req.params.id);
+    const hotel = await HotelModel.findById(req.params.id).populate('reviews');
     res.render('hotels/show', { hotel });
 }))
-
 // edit hotel get route
 app.get('/hotels/:id/edit', catchAsync(async (req, res) => {
     const hotel = await HotelModel.findById(req.params.id);
     res.render('hotels/edit', { hotel });
 }))
-
 // edit hotel put route
 app.put('/hotels/:id', validateHotel, catchAsync(async (req, res) => {
     const { id } = req.params;
     const hotel = await HotelModel.findByIdAndUpdate(id, { ...req.body.hotel });
     res.redirect(`/hotels/${hotel._id}`);
 }))
-
+// delete hotel route
 app.delete('/hotels/:id', catchAsync(async (req, res) => {
     const { id } = req.params;
     await HotelModel.findByIdAndDelete(id);
     res.redirect('/hotels');
+}))
+// post review route
+app.post('/hotels/:id/reviews', validateReview, catchAsync(async (req, res) => {
+    const hotel = await HotelModel.findById(req.params.id);
+    const review = new Review(req.body.review);
+    hotel.reviews.push(review);
+    await review.save();
+    await hotel.save();
+    res.redirect(`/hotels/${hotel._id}`);
+}))
+// delete review route
+app.delete('/hotels/:id/reviews/:reviewId', catchAsync(async (req, res) => {
+    const { id, reviewId } = req.params;
+    await HotelModel.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
+    await Review.findByIdAndDelete(reviewId);
+    res.redirect(`/hotels/${id}`)
 }))
 
 // catch non existing url and throw ExpressError to next
--- models/hotel.js
+++ models/hotel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const Review = require('./review');
 const Schema = mongoose.Schema;
 
 const HotelSchema = new Schema({
@@ -6,7 +7,24 @@ const HotelSchema = new Schema({
     image: String,
     price: Number,
     description: String,
-    location: String
+    location: String,
+    reviews: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Review'
+        }
+    ]
 });
+
+// post middleware to delete reviews with the hotel
+HotelSchema.post('findOneAndDelete', async function (doc) {
+    if (doc) {
+        await Review.deleteMany({
+            _id: {
+                $in: doc.reviews
+            }
+        })
+    }
+})
 
 module.exports = mongoose.model('Hotel', HotelSchema);
