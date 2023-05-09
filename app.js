dfb18a0e1e185948c8775be4c940cfcec52bf910
Taher Najim <taher.najim@yahoo.com>
2023-05-09 17:28:33 +0800

    setup the user model and configured passport

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const session = require('express-session');
 const flash = require('connect-flash');
 const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const User = require('./models/user');
 
 const hotelRoutes = require('./routes/hotels.js');
 const reviewRoutes = require('./routes/reviews.js');
@@ -15,11 +18,11 @@ mongoose.connect('mongodb://127.0.0.1:27017/room-reviews')
     .catch(error => console.log("MongoDB Error Occured.", error));
 
 const app = express();
-
+// app settings
 app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
-
+// some useful middleware
 app.use(express.urlencoded({ extended: true })); // parse req.body
 app.use(methodOverride('_method')); //set query string override
 app.use(express.static(path.join(__dirname, 'public'))); //set public assets folder
@@ -37,13 +40,26 @@ const sessionConfig = {
 app.use(session(sessionConfig));
 app.use(flash());
 
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+// flash session stuff
 app.use((req, res, next) => {
     res.locals.success = req.flash('success');
     res.locals.error = req.flash('error');
     next();
 })
 
-// routes
+app.get('/fakeuser', async (req, res) => {
+    const user = new User({ email: 'colt@gmail.com', username: 'colt' });
+    const newUser = await User.register(user, 'chicken');
+    res.send(newUser);
+})
+
+// route folders
 app.use('/hotels', hotelRoutes);
 app.use('/hotels/:id/reviews', reviewRoutes);
 
@@ -55,7 +71,7 @@ app.get('/', (req, res) => {
 app.all('*', (req, res, next) => {
     next(new ExpressError('Page Not Found', 404))
 })
-
+// default catch error route
 app.use((err, req, res, next) => {
     const { statusCode = 500 } = err;
     if (!err.message) err.message = 'Something went wrong...';
