dfc3c52994d3a5dd36dad677ed8823a787e90c62
Taher Najim <taher.najim@yahoo.com>
2023-05-10 17:43:50 +0800

    updated hotel model to have author and updated seeds accordingly

--- seeds/index.js
+++ seeds/index.js
@@ -17,6 +17,7 @@ const seedDB = async () => {
         const random1000 = Math.floor(Math.random() * 1000);
         const price = Math.floor(Math.random() * 20) + 10;
         const hotel = new  HotelModel({
+            author: '645af5197ce6f05a79c95393',
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
             image: 'https://source.unsplash.com/collection/4977823',
