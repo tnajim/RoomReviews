dd6d0cccc4978ce0f470a86cc87b6003e5f859b4
Taher Najim <taher.najim@yahoo.com>
2023-05-17 12:43:43 +0800

    added deleting images on the backend

--- controllers/hotels.js
+++ controllers/hotels.js
@@ -1,4 +1,5 @@
 const HotelModel = require('../models/hotel');
+const { cloudinary } = require('../cloudinary');
 
 module.exports.index = async (req, res) => {
     const hotels = await HotelModel.find({});
@@ -45,10 +46,18 @@ module.exports.renderEditForm = async (req, res) => {
 
 module.exports.updateHotel = async (req, res) => {
     const { id } = req.params;
+    console.log(req.body);
     const hotel = await HotelModel.findByIdAndUpdate(id, { ...req.body.hotel });
     const imgs = req.files.map(f => ({ url: f.path, filename: f.filename }));
     hotel.images.push(...imgs);
     await hotel.save();
+    //$pull: pulls elements out of the images array, where filename is not exactly equal to something but in deleteImages
+    if (req.body.deleteImages) {
+        for (let filename of req.body.deleteImages) {
+            await cloudinary.uploader.destroy(filename);
+        }
+        await hotel.updateOne({ $pull: { images: { filename: { $in: req.body.deleteImages } } } });
+    }
     req.flash('success', 'Successfully updated Hotel listing!');
     res.redirect(`/hotels/${hotel._id}`);
 }
--- models/hotel.js
+++ models/hotel.js
@@ -2,14 +2,19 @@ const mongoose = require('mongoose');
 const Review = require('./review');
 const Schema = mongoose.Schema;
 
+const ImageSchema = new Schema({
+    url: String,
+    filename: String,
+})
+
+// add img.thumbnail (usable property derived from db value)
+ImageSchema.virtual('thumbnail').get(function (){
+    return this.url.replace('/upload', '/upload/w_200')
+})
+
 const HotelSchema = new Schema({
     title: String,
-    images: [
-        {
-            url: String,
-            filename: String,
-        }
-    ],
+    images: [ImageSchema],
     price: Number,
     description: String,
     location: String,
