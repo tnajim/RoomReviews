c531499cba83e83f461958a6c046498b8de33774
Taher Najim <taher.najim@yahoo.com>
2023-05-18 18:21:41 +0800

    added pin popup to the map on the hotel show page

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -1,11 +1,15 @@
 mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
-  container: 'map',
-  style: 'mapbox://styles/mapbox/streets-v12',
-  center: hotel.coordinates, // starting position [lng, lat]
-  zoom: 10, // starting zoom
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v12',
+    center: hotel.geometry.coordinates, // starting position [lng, lat]
+    zoom: 10, // starting zoom
 });
 
 new mapboxgl.Marker()
-.setLngLat(hotel.coordinates)
-.addTo(map);
+    .setLngLat(hotel.geometry.coordinates)
+    .setPopup(
+        new mapboxgl.Popup({ offset: 25 })
+            .setHTML(`<h5>${hotel.title}</h5><p>${hotel.location}</p>`)
+    ) // add popup
+    .addTo(map);
