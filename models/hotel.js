7ee3bc03b22449260dd764fc93b1a6c49f1a3fc4
Taher Najim <taher.najim@yahoo.com>
2023-05-19 17:42:29 +0800

    added interactive map to the hotel index page

--- models/hotel.js
+++ models/hotel.js
@@ -2,15 +2,18 @@ const mongoose = require('mongoose');
 const Review = require('./review');
 const Schema = mongoose.Schema;
 
+//image schema for HotelSchema.images.url and filename
 const ImageSchema = new Schema({
     url: String,
     filename: String,
-})
+});
 
 // add img.thumbnail (usable property derived from db value)
 ImageSchema.virtual('thumbnail').get(function (){
     return this.url.replace('/upload', '/upload/w_200')
-})
+});
+
+const opts = { toJSON: { virtuals: true } };
 
 const HotelSchema = new Schema({
     title: String,
@@ -39,6 +42,12 @@ const HotelSchema = new Schema({
             ref: 'Review'
         }
     ]
+}, opts);
+
+HotelSchema.virtual('properties.popUpMarkup').get(function (){
+    return `
+    <strong><a href="/hotels/${this._id}">${this.title}</a></strong>
+    <p>${this.location}</p>`
 });
 
 // post middleware to delete reviews with the hotel
