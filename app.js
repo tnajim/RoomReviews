b2120604bb201ca2c59b21bf5ed59de292f562fd
Taher Najim <taher.najim@yahoo.com>
2023-05-24 11:16:44 +0800

    cleaned up unneeded console.log

--- app.js
+++ app.js
@@ -57,7 +57,6 @@ passport.deserializeUser(User.deserializeUser());
 
 // global middleware, for res.locals (have access to these in every template)
 app.use((req, res, next) => {
-    console.log(req.query);
     res.locals.currentUser = req.user;
     res.locals.success = req.flash('success');
     res.locals.error = req.flash('error');
